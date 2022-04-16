351b55b6f060c41387953abb782337c8e9d821f7
vlabon <bondar.vladyslav@chnu.edu.ua>
2022-04-16 20:48:11 +0300

    Category 'get_absolute_url'

--- app_blog/models.py
+++ app_blog/models.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 class Category(models.Model):
     category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager
     
     class Meta:
         verbose_name = u'Категрія для публікацій'
@@ -18,6 +19,14 @@ class Category(models.Model):
         
     def __str__(self):
         return self.category
+    
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/" 
+        
+        return url
     
 class Article(models.Model):
     title = models.CharField(u'Заголовок', max_length=250, help_text=u'Максимум 250 символів')
