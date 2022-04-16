a201d7dd2817254309191122bd8fc1bf18651a6d
vlabon <bondar.vladyslav@chnu.edu.ua>
2022-04-16 19:23:43 +0300

    migration #2

--- app_blog/models.py
+++ app_blog/models.py
@@ -10,10 +10,11 @@ from django.utils import timezone
 # Create your models here.
 class Category(models.Model):
     category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
+    slug = models.SlugField(u'Слаг', default='slug_default')
     
     class Meta:
-        verbose_name = u'Категрія для новини'
-        verbose_name_plural = u'Категорії для новин'
+        verbose_name = u'Категрія для публікацій'
+        verbose_name_plural = u'Категорії для публікацій'
         
     def __str__(self):
         return self.category
@@ -23,8 +24,8 @@ class Article(models.Model):
     description = models.TextField(blank=True, verbose_name=u'Опис')
     pub_date = models.DateTimeField(u'Дата публікації', default=timezone.now)
     slug = models.SlugField(u'Слаг', unique_for_date='pub_date')
-    main_page = models.BooleanField(u'Головна', default=False, help_text=u'Показувати')
-    category = models.ForeignKey(Category, related_name='news', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
+    main_page = models.BooleanField(u'Головна', default=False, help_text=u'Показувати на головній сторінці')
+    category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
     objects = models.Manager()
     
     class Meta:
